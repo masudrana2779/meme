01df67063c8e3e4379ff8d668f7a0e9beb5e1da8
imtiaj fahim <fahimimtiaj1@gmail.com>
2024-07-05 15:00:56 +0600

    Starting the work

---
+++ src/components/Partners.tsx
@@ -0,0 +1,48 @@
+import React from "react"
+import { JupiterImg } from "./Images/PartnerImageOne"
+
+const Partners = () => {
+    return (
+        <div className="flex gap-x-9 justify-center items-center">
+
+        <div className="w-[376px] h-[88px] border-2 bg-[#EAF0FF] rounded-[20px] border-[#000000] border-solid flex justify-center items-center">
+        <div className="justify-center items-center gap-x-1">
+         {/* <JupiterImg /> */}
+         <p className="not-italic font-normal leading-[18px] text-[30px] text-[#060606CC]">Jupiter</p>
+         </div>
+        </div>
+        <div className="w-[376px] h-[88px] border-2 bg-[#EAF0FF] rounded-[20px] border-[#000000] border-solid flex justify-center items-center">
+        <div className="justify-center items-center gap-x-1">
+         {/* <JupiterImg /> */}
+         <p className="not-italic font-normal leading-[18px] text-[30px] text-[#060606CC]">Raydium</p>
+         </div>
+        </div>
+        <div className="w-[376px] h-[88px] border-2 bg-[#EAF0FF] rounded-[20px] border-[#000000] border-solid flex justify-center items-center">
+        <div className="justify-center items-center gap-x-1">
+         {/* <JupiterImg /> */}
+         <p className="not-italic font-normal leading-[18px] text-[30px] text-[#060606CC]">DaxScreener</p>
+         </div>
+        </div>
+        <div className="w-[376px] h-[88px] border-2 bg-[#EAF0FF] rounded-[20px] border-[#000000] border-solid flex justify-center items-center">
+        <div className="justify-center items-center gap-x-1">
+         {/* <JupiterImg /> */}
+         <p className="not-italic font-normal leading-[18px] text-[30px] text-[#060606CC]">DexTools</p>
+         </div>
+        </div>
+        <div className="w-[376px] h-[88px] border-2 bg-[#EAF0FF] rounded-[20px] border-[#000000] border-solid flex justify-center items-center">
+        <div className="justify-center items-center gap-x-1">
+         {/* <JupiterImg /> */}
+         <p className="not-italic font-normal leading-[18px] text-[30px] text-[#060606CC]">CyberScope</p>
+         </div>
+        </div>
+        <div className="w-[376px] h-[88px] border-2 bg-[#EAF0FF] rounded-[20px] border-[#000000] border-solid flex justify-center items-center">
+        <div className="justify-center items-center gap-x-1">
+         {/* <JupiterImg /> */}
+         <p className="not-italic font-normal leading-[18px] text-[30px] text-[#060606CC]">Jupiter</p>
+         </div>
+        </div>
+        </div>
+    )
+}
+
+export default Partners
--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,18 +1,15 @@
 import Banner from "../../Banner/Banner";
-import { JokerCatAcademy } from "../../JokerCatAcademy/JokerCatAcademy";
-// import Nav from "../../Nav/Nav";
-import Partner from "../../Partner/Partner";
-import Roadmap from "../../Roadmap/Roadmap";
-import Tokenomics from "../../Tokenomics/Tokenomics";
+import Partners from "../../Partners";
 
 const Home = () => {
   return (
     <>
-      <Banner />
-      <JokerCatAcademy />
+      {/* <Banner /> */}
+     <Partners />
+      {/* <JokerCatAcademy />
       <Roadmap />
       <Tokenomics />
-      <Partner />
+      <Partner /> */}
     </>
   );
 };
