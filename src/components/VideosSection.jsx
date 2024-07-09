93c6266f561313640b17e6df874caa46640779fb
masudrana2779 <masudrana2779@gmail.com>
2024-07-09 12:40:53 +0600

    more changed

--- src/components/VideosSection.jsx
+++ src/components/VideosSection.jsx
@@ -1,9 +1,3 @@
-// const videos = [
-//   { title: "Video 1", thumbnail: "/img/v.png" },
-//   { title: "Video 2", thumbnail: "/img/v.png" },
-//   { title: "Video 3", thumbnail: "/img/v.png" },
-//   { title: "Video 4", thumbnail: "/img/v.png" },
-// ];
 
 const VideosSection = () => {
   return (
@@ -11,21 +5,6 @@ const VideosSection = () => {
       <div className="container">
         <img src="/img/vd.png" alt="" />
       </div>
-      {/* <div className="container">
-        <div className="px-[180px] bg-center py-10 bg-[url('/img/fram.png')] bg-contain bg-no-repeat">
-          <div className="grid grid-cols-2 md:grid-cols-2 gap-12">
-            {videos.map((video, index) => (
-              <div key={index} className="cursor-pointer w-full">
-                <img
-                  src={video.thumbnail}
-                  alt={video.title}
-                  className="object-fill w-full "
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
