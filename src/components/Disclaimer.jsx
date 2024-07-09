62a5cc677d5ab04dbe93331db013aa77920a53ec
masudrana2779 <masudrana2779@gmail.com>
2024-07-09 12:52:18 +0600

    more changed

--- src/components/Disclaimer.jsx
+++ src/components/Disclaimer.jsx
@@ -1,9 +1,9 @@
 const Disclaimer = () => {
   return (
     <div
-      className="bg-[#26313E] pt-24 pb-12 rounded-t-[90px] border-t-2 border-[rgba(255,255,255,0.5)]"
+      className="bg-[#26313E] pt-10 md:pt-24 pb-12 rounded-t-[90px] border-t-2 border-[rgba(255,255,255,0.5)]"
     >
-      <div className="container flex flex-col md:flex-row items-start space-x-40">
+      <div className="container flex flex-col md:flex-row items-start md:space-x-40">
         <div className="mb-4 md:mb-0 md:mr-8">
           <img
             src="/img/flogo.png"
@@ -11,7 +11,7 @@ const Disclaimer = () => {
             className="mb-[18px]"
           />
           <div className="ml-4">
-            <h4 className="text-4xl font-bold mb-5 text-white">Follow Us!</h4>
+            <h4 className="text-2xl md:text-4xl font-bold mb-5 text-white">Follow Us!</h4>
             <div className="flex space-x-4 mt-2">
               <img src="/img/s1.png" alt="" />
               <img src="/img/s2.png" alt="" />
@@ -19,13 +19,13 @@ const Disclaimer = () => {
           </div>
         </div>
         <div className="flex-1 text-white">
-          <h4 className="text-[40px] font-bold mb-2">DISCLAIMERS</h4>
-          <p className="font-normal text-xl mb-9">
+          <h4 className="md:text-[40px] text-2xl font-bold mb-2">DISCLAIMERS</h4>
+          <p className="font-thin text-base md:text-xl mb-9 opacity-70">
             $BLNDR is a meme coin with no intrinsic value or expectation of
             financial return. There is no formal team or roadmap. The coin is
             for entertainment purposes only.
           </p>
-          <p className="font-normal text-xl">
+          <p className="font-thin text-base md:text-xl opacity-70">
             The $BLNDR coin isn’t linked to any real-world brand or product.
             It&apos;s just a tribute to a beloved and well-known pawsomesauce.
           </p>
